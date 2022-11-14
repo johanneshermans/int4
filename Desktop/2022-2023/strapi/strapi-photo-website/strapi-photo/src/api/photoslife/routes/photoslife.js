6fda2d120c1b41650016e654ad4fe08baf622395
Erin Troch <erin.troch@student.howest.be>
2022-11-14 12:06:11 +0100

    Initial Commit

---
+++ Desktop/2022-2023/strapi/strapi-photo-website/strapi-photo/src/api/photoslife/routes/photoslife.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photoslife router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::photoslife.photoslife');
---
+++ Desktop/2022-2023/strapi/strapi-photo-website/strapi-photo/src/api/photoslife/services/photoslife.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * photoslife service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::photoslife.photoslife');
